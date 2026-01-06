ef738329953ea4b26089e893471f56b6442c5c81
Huyen Tran <127302084+camel-lion-child@users.noreply.github.com>
2026-01-06 10:23:49 +0100

    Remove comments for extraction and transformation steps

--- src/main.py
+++ src/main.py
@@ -5,14 +5,13 @@ from transform import transform
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 def main():
-    # 1) Extract
+
     query_id = 5493069
     df_raw = run_dune_query(query_id=query_id)
     raw_path = PROJECT_ROOT / "data" / "raw" / "aave3_cross_chain_deposits_api.csv"
     raw_path.parent.mkdir(parents=True, exist_ok=True)
     df_raw.to_csv(raw_path, index=False)
 
-    # 2) Transform
     transform()
 
 if __name__ == "__main__":
