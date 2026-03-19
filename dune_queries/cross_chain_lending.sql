e22e7337977da35a1f47b4d76559680b26b89c74
Huyen Tran <127302084+camel-lion-child@users.noreply.github.com>
2026-03-19 10:10:45 +0100

    Refactor Aave V3 deposit query for clarity and structure

--- dune_queries/cross_chain_lending.sql
+++ dune_queries/cross_chain_lending.sql
@@ -1,5 +1,12 @@
+-- This query aggregates Aave V3 deposit activity across multiple blockchains, converts token deposits into USD volume using average prices,
+-- and measures daily liquidity, users, and transactions by chain.
+
+-- Cette requête agrège l’activité de dépôts sur Aave V3 sur plusieurs blockchains, convertit les montants déposés en volume USD grâce aux prix moyens, 
+-- puis mesure la liquidité, les utilisateurs et les transactions par chaîne.
+
+-- build a unified dataset of Aave v3 deposit events across multiple blockchain
 WITH data AS (
-SELECT
+SELECT -- ETH deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -8,12 +15,12 @@ SELECT
     onBehalfOf AS users,
     evt_tx_hash AS txs
 FROM aave_v3_ethereum.Pool_evt_Supply deposits
-LEFT JOIN tokens.erc20 t ON deposits.reserve = t.contract_address AND blockchain = 'ethereum'
-WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
+LEFT JOIN tokens.erc20 t ON deposits.reserve = t.contract_address AND blockchain = 'ethereum' --join token metadata to get symbol and decimals
+WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY -- retrict data to teh selected recent time window
 
 UNION ALL
 
-SELECT
+SELECT -- Poligon (blockchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -27,7 +34,7 @@ WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 
 UNION ALL
 
-SELECT
+SELECT -- Optimism (blockchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -41,7 +48,7 @@ WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 
 UNION ALL
 
-SELECT
+SELECT -- Arbitrum (blockchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -55,7 +62,7 @@ WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 
 UNION ALL
 
-SELECT
+SELECT -- Avalance (blockchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -69,7 +76,7 @@ WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 
 UNION ALL
 
-SELECT
+SELECT -- Fantom (blochchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -83,7 +90,7 @@ WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 
 UNION ALL
 
-SELECT
+SELECT --Base (blockchain) deposits
     date_trunc('{{Time interval}}', evt_block_time) as day,
     blockchain,
     t.symbol as token,
@@ -96,7 +103,7 @@ LEFT JOIN tokens.erc20 t ON deposits.reserve = t.contract_address AND blockchain
 WHERE evt_block_time > NOW() - interval '{{Trading Num Days}}' DAY
 ),
 
-p AS (
+p AS ( -- build a price table to estimate USD deposit volume
 SELECT
     date_trunc('{{Time interval}}', minute) as day,
     blockchain,
@@ -107,7 +114,7 @@ WHERE minute > NOW() - interval '{{Trading Num Days}}' DAY
 GROUP BY 1, 2, 3
 )
 
-SELECT
+SELECT -- join deposit with token prices & aggregate final metrics by day & blockchain
     data.day,
     data.blockchain,
     SUM(amount*avg_price) AS volume,
@@ -115,5 +122,5 @@ SELECT
     COUNT(DISTINCT txs) AS txs
 FROM data
 LEFT JOIN p ON data.day = p.day AND p.contract_address = data.contract_address AND p.blockchain = data.blockchain
-GROUP BY 1, 2
+GROUP BY 1, 2 -- aggregate final results by time bucket & blockchain
 ORDER BY 1 DESC
